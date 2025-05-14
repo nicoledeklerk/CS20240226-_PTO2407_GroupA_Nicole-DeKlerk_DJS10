1babad4a35cf8aac1cc07e67d6bcf97d17707585
Nicole de Klerk <nicolenataliedeklerk@gmail.com>
2025-05-14 13:48:40 +0200

    Initialize State Variables

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -1,59 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
-function BlogPosts() {
-  const [posts, setPosts] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true); 
-
-  useEffect(() => {
-    async function fetchPosts() {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-        
-
-        if (!response.ok) {
-          throw new Error('Unable to retrieve posts');
-        }
-
-        const data = await response.json();
-        setPosts(data);
-        setLoading(false); 
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);  
-      }
-    }
-
-    fetchPosts();
-  }, []);
-
-  if (loading) {
-    return (
-      <div>
-        <p style={{ color: 'green' }}>Loading...</p>
-      </div>
-    ) 
-  }
-
-  if (error) {
-    return (
-      <div>
-        <p style={{ color: 'red'}}>Error: {error}</p>
-        <img src="./images/error-message.png" alt="Error" />
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      {posts.map(post => (
-        <article key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </article>
-      ))}
-    </div>
-  );
+function BlogPosts() { 
+    const [posts, setPosts] = useState([]); //initialize state variable posts to an empty array - stored state & function//
+    const [error, setError] = useState(null); //initialize state variable error to null//
+    const [loading, setLoading] = useState(true); //initialize state variable loading to true//
+ 
 }
 
 export default BlogPosts;
